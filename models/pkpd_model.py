eb72024b6f80df552a96e59d91f2df1cceffba20
r-chinonyelum <norosemary@gmail.com>
2025-01-27 15:28:48 +0100

    Add BayesianPKPDModel class to simulate propofol effect

--- models/pkpd_model.py
+++ models/pkpd_model.py
@@ -1,38 +1,39 @@
-class PKPDModel:
-    """
-    A pharmacokinetic/pharmacodynamic model for propofol
-    Considering factors that can add to the anesthetic effects
+import numpy as np
 
-    Attributes:
-    ec50 (float): The concentration of propofol at which 50% of the maximum effect is achieved. This helps
-    determine the potency of the drug at different concentrations.
-    gamma (float): The rate at which the effect of propofol increases with increasing concentration 
-    (the steepness of the drug response curve)
-    ke0 (float): The rate constant for drug equilibration between the plasma and the effect site.
-    This impacts the speed at which the drug reaches its maximum effect. ie the patient reaches the desired depth of anesthesia
-    
+class BayesianPKPDModel:
     """
+    Represents patient's response to propofol
 
-    def  __init__(self, ec50: float, gamma: float, ke0: float):    
-        self.ec50 = ec50
-        self.gamma = gamma
+    """
+    def __init__(self, ec50_mean: float, ec50_std: float, gamma: float, ke0: float):
+        self.ec50 = np.random.normal(ec50_mean, ec50_std) 
+        self.gamma = gamma 
         self.ke0 = ke0
         self.reset()
 
-    def reset(self):
-        "Resets the model to its initial state, representing a new patient scenario"
-        self.concentration = 0
-
-    def update(self, infusion_rate: float):
+    def update(self, infusion_rate: float) -> None:
         """
-        Updates drug concentration (model state) based on the propofol infusion rate
-        
-        Args:
-        infusion_rate (float): The rate at which propofol is being administered to the patient (mg/kg/min)
-
-        This function will simulate the change in drug concentration at the effect site, taking into account
-        factors such as absorption and metabolic clearance.
+        Update the effect site concentration based on 3-compartment pharmacokinetic model
         """
+        #Implement PK/PD dynamics
+        pass
 
-        # Pharmacokinetic calculations
+    def calculate_bis(self) -> float:
+        """
+        Calculate BIS using the sigmoid Emax model which describes the effectiveness of drug as a function of its concentration
+=
+        """
+        effect_site = self.get_effect_site_concentration()
+        return 100 * (1- (effect_site**self.gamma) / (effect_site**self.gamma + self.ec50**self.gamma))
+    
+    def get_effect_site_concentration(self) -> float:
+        """
+        Return effect site concentration
+        """
+        #implement
+        return 0.0 #placeholder
+    
+    def reset(self) -> None:
+        """Reset model to initial state"""
+        #implement
         pass
