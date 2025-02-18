8456b2b58b04117959ebfd4df5b07d3406165a8a
r-chinonyelum <norosemary@gmail.com>
2025-02-18 23:58:45 +0100

    Delete unnecessary files for simple-env

--- tests/test_environment.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from envs.anesthesia_env import AnesthesiaEnv
-
-class TestAnesthesiaEnv(unittest.TestCase):
-    def test_cognitive_load(self):
-        env = AnesthesiaEnv(config = {'shift': 'night'})
-        env.surgeries_today =3
-        env.surgery_length =120
-        self.assertAlmostEqual(env._compute_cognitive_load(), 1.0)
